22c547f8454ede7402ac071b5f70f6884284e9d0
avophile <douglas.locklin@gmail.com>
2022-09-25 18:42:40 +0300

    post articles to home page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,13 @@
 import { MetaTags } from '@redwoodjs/web'
 
+import ArticlesCell from 'src/components/ArticlesCell'
+
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       An otherworldly experience for entrepreneurs with a collaborative feel.
+      <ArticlesCell />
     </>
   )
 }
